2f0553a284dc5c01eb27579e0abf411d374a0d07
sptnod <sptnode@gmail.com>
2023-12-16 20:49:26 +0000

    wadawd

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,9 +1,17 @@
-// This is where the studio component is mounted.
-"use client";
+'use client'
 
-import { NextStudio } from "next-sanity/studio";
-import config from "@/sanity.config";
+/**
+ * This route is responsible for the built-in authoring environment using Sanity Studio.
+ * All routes under your studio path is handled by this file using Next.js' catch-all routes:
+ * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
+ *
+ * You can learn more about the next-sanity package here:
+ * https://github.com/sanity-io/next-sanity
+ */
 
-export default function Studio() {
-  return <NextStudio config={config} />;
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../../sanity.config'
+
+export default function StudioPage() {
+  return <NextStudio config={config} />
 }
